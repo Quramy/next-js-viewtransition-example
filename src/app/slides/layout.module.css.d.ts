df7f9f3bbac79f85a0bdb9480f27b99de41be934
Quramy <yosuke.kurami@gmail.com>
2025-01-17 23:32:08 +0900

    prefer className

--- src/app/slides/layout.module.css.d.ts
+++ src/app/slides/layout.module.css.d.ts
@@ -1,6 +1,8 @@
 declare const styles: {
   readonly "main": string;
   readonly "mainTransition": string;
+  readonly "normalTransition": string;
+  readonly "reverseTransition": string;
   readonly "slidein": string;
   readonly "slideout": string;
 };
